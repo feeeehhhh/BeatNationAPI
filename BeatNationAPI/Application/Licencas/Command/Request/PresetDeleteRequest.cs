60795fb6f826335838666689c12da31affd5afd3
Felipe Rocha <felipeerocha23@gmail.com>
2026-05-18 20:44:00 -0300

    remove: este commit deleta a função de presets com licensas

--- BeatNationAPI/Application/Licencas/Command/Request/PresetDeleteRequest.cs
+++
@@ -1,9 +0,0 @@
-using BeatNationAPI.Common.Responses;
-using MediatR;
-
-namespace BeatNationAPI.Application.Licencas.Command.Request
-{
-    public record PresetDeleteRequest(
-        Guid Id
-    ) : IRequest<Response<Guid>>;
-}
